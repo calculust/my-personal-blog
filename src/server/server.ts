00ecd56231375246fdcf10378b6b56b2f9211ce1
Vishal Agarwala <agarwala@me.com>
2022-02-14 17:41:55 -0500

    auth

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,9 +1,29 @@
 import express from 'express';
 import path from 'path';
+import passport from 'passport';
+import PassportLocal from 'passport-local';
 import apiRouter from './routes';
 
 const app = express();
 
+import db from './db';
+import { compareHash } from './utils/passwords';
+passport.use(new PassportLocal.Strategy({
+    usernameField: 'email'
+}, async (email, password, done) => {
+    try {
+        const [userFound] = await db.Authors.find('email', email);
+        if (userFound && compareHash(password, userFound.password)) {
+            done(null, userFound);
+        } else {
+            
+        }
+    } catch (error) {
+        done(error);
+    }
+}));
+
+app.use(passport.initialize()); 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
